b7be54da3fee932929d6139415169670b9fe612a
Lauri <lauriroomerelr24@gmail.com>
2022-06-18 13:38:34 +0300

    Initial Commit

---
+++ HeliosLoopNLearn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HeliosLoopNLearn
+//
+//  Created by Lauri Roomere on 18.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
